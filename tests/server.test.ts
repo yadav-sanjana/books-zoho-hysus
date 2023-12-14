a849e97220e6fb74d717d0fff16a527060465919
yadav-sanjana <sanjanay751@gmail.com>
2023-12-14 10:56:08 +0530

    testing configured

---
+++ tests/server.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import app from '../server';
+
+describe('Test API endpoints', () => {
+    test('should return welcome message on / GET', () => {
+        
+    });
+
+    // test('welcome', async () => {
+    //     const server = await request(app).get('/')
+    //     expect(server.body).toContain({
+    //         message: "Welcome"
+    //     })
+    // })
+
+});
+
